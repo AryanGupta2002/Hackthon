0b990e8ee0c34eec03664ed380bfa5bf35a0df8f
neeltulsiani007 <neeltulsiani007@gmail.com>
2023-03-18 02:30:23 +0530

    some styling done

--- frontend/src/components/ComporStrtup.js
+++ frontend/src/components/ComporStrtup.js
@@ -5,17 +5,27 @@ function ComporStrtup()
 {
 const navigate = useNavigate();
   return (
-    <div className = "companyorstartup">
-    <br/>
-    <h3 id="titleforcs" >Are you a comapny or a startup?<br></br>
-    </h3>
-     <button id = "companybutton" onClick={() => {navigate('/companyform');}}>Yes</button>
-     <button id = "startupbutton" onClick={() => {navigate('/startupform');}}>No</button>
-     <p id = "noforcs"
-            onClick={() => {navigate('/spdform');}}>
-            Not Either ? Click here
-     </p>
-    </div>
+    <div id = "companyorstartup" class ="flex h-screen flex-col">
+    <div class="flex flex-1 items-center justify-center">
+      <br/>
+      <h3 id="titleforcs" class="text-4xl font-medium text-center mr-4" >Are you a <br></br>
+      </h3>
+      <br></br>
+      <button id = "companybutton"class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      Company
+    </button>
+    <span id = "or" class ="text-4xl font-medium mr-4 ml-4">or</span>
+    <button id="startupbutton" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      Startup
+  </button>
+  <p class="ml-2"> Not either ?
+      <span id = "noforcs" class = "hover:underline cursor-pointer ml-1 hover:text-teal-700 "
+              onClick={() => {navigate('/spdform');}}>
+               Click here
+      </span>
+      </p>
+      </div>
+       </div>
   )
 }
  export default ComporStrtup
