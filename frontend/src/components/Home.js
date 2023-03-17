fb8dc63b633631a1b128a9e47531c5c4c6b2111c
Aryan Gupta <guptaaryan2580@gmail.com>
2023-03-18 03:50:06 +0530

    nav bar and home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,22 +1,210 @@
-import React from 'react'
-import {useEffect,useState} from 'react'
-
+import React from "react";
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import Profile from "./dummy.png";
+import Avatar from "@mui/material/Avatar";
+import x from "./2.jpg"
+import y from "./3.jpg"
+import z from "./download.jpg"
 
 function Home() {
-    const [user, setUser ] = useState("")
-    
-    console.log(user) 
+  const [user, setUser] = useState("");
 
-    useEffect(() => {
-        setUser(JSON.parse(localStorage.getItem("user")))
-        console.log(user)
-      }, [])
-    
+  console.log(user);
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+    console.log(user);
+  }, []);
+
   return (
-    <div>
-    <h1>Home is here</h1>
+    <div id="home" className="w-[100vw] h-[100vh] bg-gray-200">
+      <div>
+        <NavBar />
+      </div>
+      <div className="flex">
+        <div className="left w-[25%]">
+          <div className="w-[80%] m-4 ">
+            <div className="bg-white rounded">
+              <ul className="text-center content-center">
+                <p className="text-3xl font-serif">Professors</p>
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={Profile}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={x}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={y}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={z}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+              </ul>
+            </div>
+            <div className=" bg-white mt-8 rounded">
+              <ul className="text-center content-center">
+                <p className="text-3xl font-serif">Peers</p>
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={z}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={x}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={Profile}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={y}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+              </ul>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="center w-[50%]">
+
+        </div>
+
+
+        <div className="right w-[25%]">
+        <div className="w-[80%] m-4 ">
+            <div className="bg-white rounded">
+              <ul className="text-center content-center">
+                <p className="text-3xl font-serif">Companies</p>
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={z}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={x}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={Profile}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={z}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+              </ul>
+            </div>
+            <div className=" bg-white mt-8 rounded">
+              <ul className="text-center content-center">
+                <p className="text-xl font-serif">Trending Startups</p>
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={z}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center text">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={x}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={z}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+                <li className="pt-1 pb-1 flex items-center justify-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={x}
+                    sx={{ width: 24, height: 24 }}
+                  />{" "}
+                  <p className="pl-2 mb-2">Aryan Gupta</p>
+                </li>
+                <hr />
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
