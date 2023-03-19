4bd7ae37a3d660525d98ba03e499f84ada107060
Aryan Gupta <guptaaryan2580@gmail.com>
2023-03-19 07:29:31 +0530

    push

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -63,7 +63,10 @@ function Post({post,user}) {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
             </svg>
           </span>
+          { post.type == "Hiring"?  
           <span onClick={handlemodal}>Applicants</span>
+          :(null)
+          }
         </div>
         <div class="flex space-x-1 items-center">
           <span>
